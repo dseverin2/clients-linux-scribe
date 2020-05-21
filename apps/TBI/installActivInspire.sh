ae815dfcfe9d307c3cbb99dfa3ac87bacb64c392
SEVERIN Didier <65120153+dseverin2@users.noreply.github.com>
2020-05-21 14:06:04 +0400

    Add files via upload

--- apps/TBI/installActivInspire.sh
+++ apps/TBI/installActivInspire.sh
@@ -1,12 +1,20 @@
 #!/bin/sh
-# Ajout du dépot
+# Ajout du dépot application ActivBoard
 echo deb http://activsoftware.co.uk/linux/repos/ubuntu bionic non-oss >> /etc/apt/sources.list
 wget http://activsoftware.co.uk/linux/repos/driver/PrometheanLtd.asc
 apt-key add ./PrometheanLtd.asc
 rm -fr ./PrometheanLtd.asc
 
+# Ajout du dépot drivers ActivDriver & ActivTools
+echo deb http://activsoftware.co.uk/linux/repos/driver/ubuntu bionic oss non-oss >> /etc/apt/sources.list
+wget http://activsoftware.co.uk/linux/repos/driver/PrometheanLtd.asc
+apt-key add ./PrometheanLtd.asc
+rm -fr ./PrometheanLtd.asc
+
+
 # Installation
 apt update
+apt install activdriver activtools
 apt install activ-meta-fr -y
 apt --fix-broken install -y
 apt autoremove
