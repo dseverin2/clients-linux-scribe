57b5b94308cd796bc2f88b3d013de658e06f1426
SEVERIN Didier <65120153+dseverin2@users.noreply.github.com>
2020-09-22 10:59:09 +0400

    Update installOpenBoard.sh

--- apps/installOpenBoard.sh
+++ apps/installOpenBoard.sh
@@ -15,7 +15,7 @@ elif [ "$1" = "bionic" ]; then
 	echo "deb http://fr.archive.ubuntu.com/ubuntu/ xenial universe" >> /etc/apt/sources.list
 	echo "deb http://fr.archive.ubuntu.com/ubuntu/ xenial-updates universe" >> /etc/apt/sources.list
 	sudo apt update
-	sudo apt install libavformat-ffmpeg56
+	sudo apt install libavformat-ffmpeg56 -y
 	wget https://github.com/OpenBoard-org/OpenBoard/releases/download/v1.5.4/openboard_ubuntu_16.04_1.5.4_amd64.deb
 	sudo gdebi openboard_ubuntu_16.04_1.5.4_amd64.deb
 	mv /etc/apt/sources.list.back /etc/apt/sources.list
