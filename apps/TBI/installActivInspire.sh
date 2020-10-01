ea245f79818b8af3b50db58d1ea536fd1be4420c
dseverin <dseverin2@ac-reunion.fr>
2020-10-01 22:40:46 +0400

    maj activinspire

--- apps/TBI/installActivInspire.sh
+++ apps/TBI/installActivInspire.sh
@@ -25,9 +25,9 @@ sudo dpkg -i libicu60_60.2-3ubuntu3.1_amd64.deb
 sudo apt-get install gsettings-ubuntu-schema
 
 # Rendre ActivDriver fonctionnel / Nécessite le downgrade vers 4.19
-cd /tmp/
-wget -c http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.19/linux-headers-4.19.0-041900_4.19.0-041900.201810221809_all.deb
-wget -c http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.19/linux-headers-4.19.0-041900-generic_4.19.0-041900.201810221809_amd64.deb
-wget -c http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.19/linux-image-unsigned-4.19.0-041900-generic_4.19.0-041900.201810221809_amd64.deb
-wget -c http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.19/linux-modules-4.19.0-041900-generic_4.19.0-041900.201810221809_amd64.deb
-sudo dpkg -i *.deb
+#cd /tmp/
+#wget -c http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.19/linux-headers-4.19.0-041900_4.19.0-041900.201810221809_all.deb
+#wget -c http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.19/linux-headers-4.19.0-041900-generic_4.19.0-041900.201810221809_amd64.deb
+#wget -c http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.19/linux-image-unsigned-4.19.0-041900-generic_4.19.0-041900.201810221809_amd64.deb
+#wget -c http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.19/linux-modules-4.19.0-041900-generic_4.19.0-041900.201810221809_amd64.deb
+#sudo dpkg -i *.deb
