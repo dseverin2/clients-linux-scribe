0a62093c290a5f3f319e452e6599cb8ca23010fa
SEVERIN Didier <65120153+dseverin2@users.noreply.github.com>
2020-10-11 00:58:53 +0400

    MàJ libssl, curl, libicu

--- apps/TBI/installActivInspire.sh
+++ apps/TBI/installActivInspire.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+# v1.0 - 11.10.20
+# Didier SEVERIN
+
 # Ajout du dépot application ActivBoard
 echo "deb http://activsoftware.co.uk/linux/repos/ubuntu bionic non-oss" | tee /etc/apt/sources.list.d/promethean-activboard.list
 
@@ -12,14 +15,19 @@ rm -fr ./PrometheanLtd.asc
 wget http://security.ubuntu.com/ubuntu/pool/main/o/openssl1.0/libssl1.0.0_1.0.2n-1ubuntu5.4_amd64.deb
 dpkg -i libssl1.0.0_1.0.2n-1ubuntu5.4_amd64.deb
 
-# Installation
+# Installation de libcurl 3 et 4
+add-apt-repository ppa:xapienz/curl34 -y
 apt update
-apt install activdriver activtools -y
-apt install activ-meta-fr -y
-apt install --fix-broken -y
-apt autoremove
+apt install curl -y
 
-# Rendre ActivInspire fonctionnel
+# Installation des librairies utilisées au lancement d'ActivInspire
 wget http://security.ubuntu.com/ubuntu/pool/main/i/icu/libicu60_60.2-3ubuntu3.1_amd64.deb
 sudo dpkg -i libicu60_60.2-3ubuntu3.1_amd64.deb
 sudo apt-get install gsettings-ubuntu-schemas
+
+# Installation
+apt install activdriver activtools -y
+apt install activ-meta-fr -y
+apt install --fix-broken -y
+apt autoremove -y
+
