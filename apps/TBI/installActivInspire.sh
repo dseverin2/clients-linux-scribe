383004a3ba1fc20e85d778b32cd565076a10a8e9
SEVERIN Didier <65120153+dseverin2@users.noreply.github.com>
2020-10-11 00:49:55 +0400

    Mise à jour de libssl et curl34

--- apps/TBI/installActivInspire.sh
+++ apps/TBI/installActivInspire.sh
@@ -1,33 +1,25 @@
 #!/bin/sh
 # Ajout du dépot application ActivBoard
-echo deb http://activsoftware.co.uk/linux/repos/ubuntu bionic non-oss >> /etc/apt/sources.list
-wget http://activsoftware.co.uk/linux/repos/driver/PrometheanLtd.asc
-apt-key add ./PrometheanLtd.asc
-rm -fr ./PrometheanLtd.asc
+echo "deb http://activsoftware.co.uk/linux/repos/ubuntu bionic non-oss" | tee /etc/apt/sources.list.d/promethean-activboard.list
 
 # Ajout du dépot drivers ActivDriver & ActivTools
-echo deb http://activsoftware.co.uk/linux/repos/driver/ubuntu bionic oss non-oss >> /etc/apt/sources.list
+echo "deb http://activsoftware.co.uk/linux/repos/driver/ubuntu bionic oss non-oss" | tee /etc/apt/sources.list.d/promethean-activdriver.list
 wget http://activsoftware.co.uk/linux/repos/driver/PrometheanLtd.asc
 apt-key add ./PrometheanLtd.asc
 rm -fr ./PrometheanLtd.asc
 
+# Installation de libssl
+wget http://security.ubuntu.com/ubuntu/pool/main/o/openssl1.0/libssl1.0.0_1.0.2n-1ubuntu5.4_amd64.deb
+dpkg -i libssl1.0.0_1.0.2n-1ubuntu5.4_amd64.deb
 
 # Installation
 apt update
-apt install activdriver activtools
+apt install activdriver activtools -y
 apt install activ-meta-fr -y
-apt --fix-broken install -y
+apt install --fix-broken -y
 apt autoremove
 
 # Rendre ActivInspire fonctionnel
 wget http://security.ubuntu.com/ubuntu/pool/main/i/icu/libicu60_60.2-3ubuntu3.1_amd64.deb
 sudo dpkg -i libicu60_60.2-3ubuntu3.1_amd64.deb
 sudo apt-get install gsettings-ubuntu-schemas
-
-# Rendre ActivDriver fonctionnel / Nécessite le downgrade vers 4.19
-#cd /tmp/
-#wget -c http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.19/linux-headers-4.19.0-041900_4.19.0-041900.201810221809_all.deb
-#wget -c http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.19/linux-headers-4.19.0-041900-generic_4.19.0-041900.201810221809_amd64.deb
-#wget -c http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.19/linux-image-unsigned-4.19.0-041900-generic_4.19.0-041900.201810221809_amd64.deb
-#wget -c http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.19/linux-modules-4.19.0-041900-generic_4.19.0-041900.201810221809_amd64.deb
-#sudo dpkg -i *.deb
