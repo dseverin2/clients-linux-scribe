47433e68275e22d22fe1b49b3c3e1e335a56bcf7
SEVERIN Didier <65120153+dseverin2@users.noreply.github.com>
2020-11-11 21:53:21 +0400

    Update installActivInspire.sh

--- apps/TBI/installActivInspire.sh
+++ apps/TBI/installActivInspire.sh
@@ -36,8 +36,12 @@ sudo rm -f libssl*.deb libicu*.deb PrometheanLtd.asc
 # Compilation du driver pour les ActivBoard de 1ère et 2ème génération
 read -p "Cet ordinateur sera-t-il connecté à un ActivBoard de 1ère ou 2ème génération ? O/N " reponse
 if [ "$reponse"=="o" ] || [ "$reponse"=="O" ]; then
+  kernelversion=$(uname -r)
+  if [[ $kernelversion == 5*]]; then
+    echo "Modification du Makefile des drivers pour compatibilité avec kernels 5.*"
+    sed -i -e "s/SUBDIRS/M/g" /usr/src/promethean/kernel/Makefile
+  fi
   echo "Compilation des drivers"
-  sed -i -e "s/SUBDIRS/M/g" /usr/src/promethean/kernel/Makefile
   cd /usr/src/promethean/kernel/
   ./b
 fi
