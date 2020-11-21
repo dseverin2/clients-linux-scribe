797d0bfb5fb09af60413e749cebcba82e8dd3bcd
SEVERIN Didier <65120153+dseverin2@users.noreply.github.com>
2020-11-21 17:12:08 +0400

    Update Param_proxy.sh

--- Param_proxy.sh
+++ Param_proxy.sh
@@ -43,7 +43,7 @@ port=$proxy_def_port" > /usr/share/glib-2.0/schemas/my-defaults.gschema.override
 	######################################################################
 	writelog "---Inscription du proxy dans /etc/environment"
 	sed '/[h|f|n][a-z]*_proxy*/d' /etc/environment | tee /etc/environment1 # Nettoyage en cas de réintégration directe
-	addtoend /etc/environment "http_proxy=http://$proxy_def_ip:$proxy_def_port/" "https_proxy=http://$proxy_def_ip:$proxy_def_port/" "ftp_proxy=http://$proxy_def_ip:$proxy_def_port/" "no_proxy=\"$proxy_env_noproxy\"" 2>> $logfile
+	addtoend /etc/environment1 "http_proxy=http://$proxy_def_ip:$proxy_def_port/" "https_proxy=http://$proxy_def_ip:$proxy_def_port/" "ftp_proxy=http://$proxy_def_ip:$proxy_def_port/" "no_proxy=\"$proxy_env_noproxy\"" 2>> $logfile
 	mv /etc/environment /etc/environment.bck
 	mv /etc/environment1 /etc/environment
 	
