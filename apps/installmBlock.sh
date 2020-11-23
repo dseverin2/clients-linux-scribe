07f4a5e8211b7537b6e62a6f325bf6002ec2d8a9
dseverin <dseverin2@ac-reunion.fr>
2020-11-24 00:37:45 +0400

    up

--- apps/installmBlock.sh
+++ apps/installmBlock.sh
@@ -25,10 +25,7 @@ apt install -fy
 rm -f mBlock_4.0.4_amd64.deb mLink-1.2.0-amd64.deb
 
 # Installation des librairies manquantes
-if grep LinuxMint /etc/lsb-release; then
-	apt-get install libpangoft2-1.0-0 libpangocairo-1.0-0 libpango-1.0-0 -y
-	unzip mBlock.zip -d /opt/makeblock/mBlock
-fi
+unzip mBlock.zip -d /opt/makeblock/mBlock
 
 #Installation des librairies pour arduino
 cd /usr/share/arduino/lib || exit
